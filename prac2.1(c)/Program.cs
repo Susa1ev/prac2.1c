9b7bdb0405c80516dedd4ee11e61edd624261e54
Susa1ev <susalytt@gmail.com>
2021-11-30 17:29:58 +0300

    Добавьте файлы проекта.

---
+++ prac2.1(c)/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace prac2._1_c_
+{
+    class Threeug
+    {
+        private double x = 0;
+        private double y = 0;
+        public Threeug()
+        {
+            input();
+        }
+        public void input()
+        {
+            Console.Write("Введи x "); 
+            if(Double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Введи y ");
+                if (Double.TryParse(Console.ReadLine(), out y))
+                {
+                    sravnenie();
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели что-то не то");
+                    input();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то");
+                input();
+            }
+
+        }
+        public void sravnenie()
+        {
+            if(x < 0 && x > -23 && y < 0 && y > x)
+            {
+                Console.Write("\tНаходится внутри функций\n");
+                input();
+            }
+            else if (x == 0 || x == -23 || y == 0)
+            {
+                Console.Write("\tНаходится на функциях\n");
+                input();
+            }
+            else
+            {
+                Console.Write("\tНаходится вне функций\n");
+                input();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Threeug threeug = new Threeug();
+        }
+    }
+}
